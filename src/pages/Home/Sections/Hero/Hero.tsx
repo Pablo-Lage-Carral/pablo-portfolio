c10470fafa0d924dab1c123eb12a6d5403ba42d5
Pablo Lage Carral <pablo.carral@outlook.com>
2024-09-09 07:08:18 -0300

    retirando Theme

--- src/pages/Home/Sections/Hero/Hero.tsx
+++ src/pages/Home/Sections/Hero/Hero.tsx
@@ -6,7 +6,6 @@ import { AnimatedBackground } from "../../../../components/AnimatedBackground/An
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import theme from "../../../../theme";
 
 const   Hero = () => {
 
